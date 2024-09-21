4a46e8d4ed8ae17388e9dbe487ed1d34e2cfae59
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-09-21 15:35:13 +0530

    change moon icon

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,4 +1,5 @@
-import { Moon, Sun } from 'lucide-react'
+import { MoonStar, Sun } from 'lucide-react'
+// import { BsMoonStarsFill } from "react-icons/bs";
 
 import { Button } from '@src/components/ui/button'
 import {
@@ -17,7 +18,7 @@ export function ModeToggle() {
             <DropdownMenuTrigger asChild>
                 <Button variant="outline" size="icon">
                     <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <MoonStar className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                     <span className="sr-only">Toggle theme</span>
                 </Button>
             </DropdownMenuTrigger>
