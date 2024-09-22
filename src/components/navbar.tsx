e53e862500dd0d3daaee290b27fc9d761460871a
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-09-22 16:41:48 +0530

    navbar changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,32 +3,32 @@ import { NavigationMenu, NavigationMenuLink } from './ui/navigation-menu'
 import { ModeToggle } from './mode-toggle'
 const navbar = () => {
     return (
-        <>
-            <nav className="bg-black p-3">
-                <div className="absolute">
+        <nav className="bg-black p-3">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="flex items-center">
                     <ModeToggle />
                 </div>
-                <div className="container mx-auto flex justify-center">
-                    <div className="flex space-x-4">
-                        <NavigationMenu>
-                            <NavigationMenuLink
-                                href="/"
-                                className="rounded-md px-4 py-2 bg-black text-white hover:bg-white hover:text-black"
-                            >
-                                Home
-                            </NavigationMenuLink>
-                            <NavigationMenuLink
-                                href="/test"
-                                className="rounded-md px-4 py-2 bg-black text-white hover:bg-white hover:text-black"
-                            >
-                                About
-                            </NavigationMenuLink>
-                        </NavigationMenu>
-                    </div>
+                <div className="flex items-center">
+                    <img src="../../logo cropped.svg" alt="Logo" className="h-16" />
                 </div>
-            </nav>
-        </>
+                <div className="flex space-x-4">
+                    <NavigationMenu>
+                        <NavigationMenuLink
+                            href="/"
+                            className="rounded-md px-4 py-2 bg-black text-white hover:bg-white hover:text-black"
+                        >
+                            Home
+                        </NavigationMenuLink>
+                        <NavigationMenuLink
+                            href="/test"
+                            className="rounded-md px-4 py-2 bg-black text-white hover:bg-white hover:text-black"
+                        >
+                            About
+                        </NavigationMenuLink>
+                    </NavigationMenu>
+                </div>
+            </div>
+        </nav>
     )
 }
-
 export default navbar
